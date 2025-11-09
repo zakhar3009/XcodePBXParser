bf289af22e0ecbf951df0a113102aaca23c5c792
zakharlit <zakhlit@gmail.com>
2025-11-09 12:13:32 +0200

    Define pbxproj AST structs

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,26 @@ use pest_derive::Parser;
 #[grammar = "src/pbxproj.pest"]
 struct PbxprojParser;
 
+#[derive(Debug, Clone, PartialEq)]
+pub struct PbxDocument {
+    pub root: PbxValue,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum PbxValue {
+    Dictionary(Vec<PbxEntry>),
+    Array(Vec<PbxValue>),
+    String(String),
+    Identifier(String),
+    Number(String),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct PbxEntry {
+    pub key: String,
+    pub value: PbxValue,
+}
+
 pub fn parse_document(source: &str) -> Result<(), pest::error::Error<Rule>> {
     PbxprojParser::parse(Rule::file, source).map(|_| ())
 }
